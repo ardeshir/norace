a0a7329f9c0fa3dd8b06d9d6a083d8d0eddedc86
Ardeshir Sepahsalar <ardeshir.org@gmail.com>
2016-06-08 20:47:33 -0500

    adding mutex

--- main_broke.go
+++ main_broke.go
@@ -10,13 +10,12 @@ import (
 
 var balance int
 var transactionNo int
-var mutx sync.Mutex
 
 func main() {
 	rand.Seed(time.Now().Unix())
 	runtime.GOMAXPROCS(2)
 	var wg sync.WaitGroup
-	balanceChan := make(chan int)
+
 	tranChan := make(chan bool)
 
 	balance = 1000
@@ -24,63 +23,27 @@ func main() {
 	fmt.Println("Staring balance: $", balance)
 
 	wg.Add(1)
-
 	for i := 0; i < 100; i++ {
-		go func(ii int) {
-
+		go func(ii int, trChan chan (bool)) {
 			transactionAmount := rand.Intn(25)
-			balanceChan <- transactionAmount
-
+			transaction(transactionAmount)
 			if ii == 99 {
-				fmt.Println("Should be quitting time")
-				tranChan <- true
-				close(balanceChan)
-				wg.Done()
+				trChan <- true
 			}
-		}(i)
+		}(i, tranChan)
 	}
 	go transaction(0)
-
-	breakPoint := false
-
-	for {
-
-		if breakPoint == true {
-			break
-		}
-
-		select {
-
-		case amt := <-balanceChan:
-			fmt.Println("Transaction for $", amt)
-			if (balance - amt) < 0 {
-				fmt.Println("Transaction failed!")
-			} else {
-				balance = balance - amt
-				fmt.Println("Transaction succeeded")
-			}
-			fmt.Println("Balance now $", balance)
-
-		case status := <-tranChan:
-			if status == true {
-
-				fmt.Println("Done")
-				breakPoint = true
-				close(tranChan)
-
-			}
-
-		} // end select
-	} // end for
-
+	select {
+	case <-tranChan:
+		fmt.Println("Transactions finished")
+		wg.Done()
+	}
 	wg.Wait()
+	close(tranChan)
 	fmt.Println("Final balance: $", balance)
-
-} // end of main
+}
 
 func transaction(amt int) bool {
-	mutx.Lock()
-
 	approved := false
 	if (balance - amt) < 0 {
 		approved = false
@@ -97,7 +60,5 @@ func transaction(amt int) bool {
 	transactionNo = transactionNo + 1
 	fmt.Println(transactionNo, "Transaction for $", amt, approvedText)
 	fmt.Println("\tRemaining balance $", balance)
-
-	mutx.Unlock()
 	return approved
 }
